586b72978a5a382e14e73e533ad0de03cdea4ca3
zhaoshenghao <1213988947@qq.com>
2018-03-14 00:15:46 +0800

    loveseven

--- src/js/common.js
+++ src/js/common.js
@@ -1,24 +1,33 @@
 'use strict'
 
+function lun() {var mySwiper = new Swiper('.swiper-container', {
+    // direction: 'vertical',     纵向了
+    loop: false,
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    autoplay: true
+});
+}
 var love = $('#love')
 var no_love = $('#no_love')
 
 love.click(function(){
-    $('#form').css('display','block');  
+    $('#form1').css('display','block');
+    $('#form').css('display','block');
+    $('.like').css('display','none');
+    lun();
+    
 });
 
 no_love.click(function(){
     $('#form1').css('display','block');
     $('#form2').css('display','block');
+    $('.like').css('display','none');
+    lun();
 });
 
-$('.yes').click(function(){
-    $('#form').css('display','block');
-    $('#form1').css('display','none');
-    $('#form2').css('display','none');
-})
 
-$('.no').click(function(){
-    $('#form1').css('display','none'); 
-    $('#form2').css('display','none'); 
-})
